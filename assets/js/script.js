2ad75af8451dc824e7f52b9b1ef53cd9ecefe94e
Rick South <38502394+ricksouth@users.noreply.github.com>
2021-02-05 00:24:58 +0100

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,32 @@
 $(document).ready(function(e) {
-	console.log("hi");
+	loadJsonData($("#mcversions").val());
+});
+
+function loadJsonData(version) {
+	$.ajax({
+		url: "https://raw.githubusercontent.com/ricksouth/eventlistforge.com/main/json/pre/" + version + ".json",
+		type: "GET",
+		dataType: 'json',
+		success: function(data){
+			var html = "<h2>Showing all events available in Forge for Minecraft version " + $("#mcversions option:selected").text() + ".</h2>";
+
+			var packages = data[0];
+			for (var packagename in packages) {
+				var package = packages[packagename];
+				html += "<h3>" + packagename + "</h3>";
+				for (var i = 0; i < package.length; i++) {
+					var event = package[i];
+					html += "<pre>" + event + "</pre>";
+				}
+			}
+
+			$("#content").html(html);
+		},
+		error: function(data) { }
+	});
+}
+
+$("#mcversions").on('change', function(e) {
+	var version = $(this).val();
+	loadJsonData(version);
 });
